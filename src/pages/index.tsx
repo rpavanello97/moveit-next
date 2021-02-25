616571ca8a3df67b98538eeb2db0058b5099e2bf
Rafael Bertolim Pavanello <rpavanello97@hotmail.com>
2021-02-24 23:00:22 -0300

    class three finished

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,10 +2,12 @@ import { CompletedChallanges } from "../components/CompletedChallenges";
 import { Countdown } from "../components/Countdown";
 import { ExperienceBar } from "../components/ExperienceBar";
 import { Profile } from "../components/Profile";
+import { ChallengeBox } from "../components/ChallengeBox";
 
 import Head from 'next/head';
 
 import styles from '../styles/pages/Home.module.css';
+
 
 export default function Home() {
   return (
@@ -22,7 +24,7 @@ export default function Home() {
           <Countdown />
         </div>
         <div>
-
+          <ChallengeBox />
         </div>
       </section>
     </div>
